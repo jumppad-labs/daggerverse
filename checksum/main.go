a166afb0ebbd9f77d99f676bccd8854b8a3a0c2b
Nic Jackson <jackson.nic@gmail.com>
2024-02-27 11:32:52 +0000

    Update checksum to add local file capability

--- checksum/main.go
+++ checksum/main.go
@@ -9,13 +9,29 @@ import (
 type Checksum struct{}
 
 // CalculateChecksum will calculate the checksum of a given URL
-func (c *Checksum) CalculateChecksum(ctx context.Context, url string) (string, error) {
+func (c *Checksum) CalculateFromURL(ctx context.Context, url string) (string, error) {
 	str, err := dag.Container().
 		From("alpine:latest").
 		WithExec([]string{"apk", "add", "curl"}).
 		WithExec([]string{
 			"curl", "-sLo", "/jumppad", url,
 		}).
+		WithExec([]string{
+			"sha256sum", "-b", "/jumppad",
+		}).
+		Stdout(ctx)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to calculate checksum: %w", err)
+	}
+
+	return strings.TrimSpace(str), nil
+}
+
+func (c *Checksum) CalculateFromFile(ctx context.Context, file *File) (string, error) {
+	str, err := dag.Container().
+		From("alpine:latest").
+		WithFile("/jumppad", file).
 		WithExec([]string{
 			"sha256sum", "-b", "/jumppad",
 		}).
